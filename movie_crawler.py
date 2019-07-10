3f0757c6ea12d599fed6c461cb726d303d85b6c1
s0wnd <aimmvp@gmail.com>
2019-07-10 22:23:25 +0900

    apscheduler 를 이용한 주기적인 메세지 전송 및 정지(pause) 기능 적용)

--- movie_crawler.py
+++ movie_crawler.py
@@ -2,20 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 import telegram
 import mykey
+from apscheduler.schedulers.blocking import BlockingScheduler
+
 
 myId =  mykey.MY_ID
 bot = telegram.Bot(token =  mykey.TELEGRAM_KEY)
 url = "http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20190709"
-html = requests.get(url)
-soup = BeautifulSoup(html.text, 'html.parser')
-imax = soup.select_one('span.imax')
-telegram_message = "";
-if(imax):
-    imax = imax.find_parent('div', class_ = 'col-times')
-    title = imax.select_one('div.info-movie > a > strong').text.strip()
-    telegram_message = title + ' IMAX 예매가 열렸습니다.'
 
-else: 
-    telegram_message = title + ' IMAX 예매가 아직 열리지 않았습니다.'
+def job_function():
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, 'html.parser')
+    imax = soup.select_one('span.imax')
+    telegram_message = "";
+    if(imax):
+        imax = imax.find_parent('div', class_ = 'col-times')
+        title = imax.select_one('div.info-movie > a > strong').text.strip()
+        telegram_message = title + ' IMAX 예매가 열렸습니다.'
+        sched.pause()
+    # else: 
+    #     telegram_message = title + ' IMAX 예매가 아직 열리지 않았습니다.'
 
-bot.sendMessage(chat_id =myId, text= telegram_message)     
+    bot.sendMessage(chat_id =myId, text= telegram_message)     
+
+sched = BlockingScheduler()
+sched.add_job(job_function, 'interval', seconds=30)
+sched.start()
